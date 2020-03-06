0985b068f3e5362e70d65f52c411694513540d55
Andy Velazquez <andyvelazquez13@yahoo.com>
2020-03-05 16:09:39 -0800

    basic files and make file

---
+++ assignment5/minget.c
@@ -0,0 +1,10 @@
+/* Andy Velazquez , avelaz02@calpoly.edu
+ * Caleb Rabbon
+ * Assignment 5
+ */
+
+int main(int argc, char * argv[])
+{
+
+}
+       
---
+++ assignment5/minls.c
@@ -0,0 +1,9 @@
+/* Andy Velazquez , avelaz02@calpoly.edu
+ * Caleb Rabbon
+ * Assignment 5
+ */
+
+int main(int argc, char * argv[])
+{
+	return 0;
+}
